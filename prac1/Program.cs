fb1e9590f4f9cc52ebc82e0a3989e6ac8500db14
ilya4iter <ilya.rymanov.02@mail.ru>
2020-02-17 16:10:19 +0500

    Добавьте файлы проекта.

---
+++ prac1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conStr = @"Data Source=.\SBD\MSSQL; Initial Catalog=SKSasha; Integrated Security=True;";
+
+            DataTable parentTable = new DataTable();
+            DataTable childTable = new DataTable();
+            DataColumn childColumn = childTable.Columns.Add("ChildColumn", typeof(int));
+            DataColumn parentColumn = parentTable.Columns.Add("ParentColumn", typeof(int));
+            parentTable.Cons
+
+        }
+    }
+}
